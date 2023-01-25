bbf4984aafbf6088b46005565888efe7db8bf45f
Selien Yorbandi <selienyorbandi@gmail.com>
2023-01-24 21:53:29 -0300

    feat: add spinner

---
+++ src/app/components/spinner/spinner.component.html
@@ -0,0 +1,3 @@
+<div class="spinner-border text-secondary" role="status">
+  <span class="visually-hidden">Loading...</span>
+</div>
---
+++ src/app/components/spinner/spinner.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-spinner',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './spinner.component.html',
+  styleUrls: ['./spinner.component.css']
+})
+export class SpinnerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
